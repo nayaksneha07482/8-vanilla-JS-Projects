e30061516c6739dc8aa5aba13985b3b4b396f48e
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-02-05 12:07:23 +0530

    js complete

--- 8.Quiz App/script.js
+++ 8.Quiz App/script.js
@@ -150,4 +150,81 @@ function createQuizChoices(data) {
       });
     }
   }
+  function createCorrectIcon() {
+    const iconEl = document.createElement("div");
+    iconEl.classList.add("icon");
+    iconEl.innerHTML = `<ion-icon name="checkmark-circle"></ion-icon>`;
+    return iconEl;
+  }
+  
+  function createWrongIcon() {
+    const iconEl = document.createElement("div");
+    iconEl.classList.add("icon");
+    iconEl.innerHTML = `<ion-icon name="close-circle"></ion-icon>`;
+    return iconEl;
+  }
+  
+  nextBtnEl.addEventListener("click", (e) => {
+    if (isAnswerSelected) {
+      isAnswerSelected = false;
+      nextBtnEl.disabled = true;
+  
+      if (quizCount < shuffledQuizData.length - 1) {
+        ++quizCount;
+        createQuiz();
+      } else {
+        createResultEl();
+      }
+    } else {
+      alert("you must select a choice");
+    }
+  });
+  
+  function createResultEl() {
+    quizContainerEl.innerHTML = ``;
+  
+    progressEl.style.opacity = `0`;
+  
+    const tableEl = document.createElement("table");
+    tableEl.classList.add("resultTable");
+  
+    const resultData = [
+      { text: `Total Question`, count: shuffledQuizData.length },
+      { text: `Correct`, count: result },
+      { text: `Wrong`, count: shuffledQuizData.length - result },
+      {
+        text: `Percentage`,
+        count: `${(result / shuffledQuizData.length) * 100}%`,
+      },
+      { text: `Your Total`, count: `${result} / ${shuffledQuizData.length}` },
+    ];
+  
+    // tr1
+    function createTableRow(data) {
+      const tr = document.createElement("tr");
+      const th = document.createElement("th");
+      th.innerHTML = data.text;
+      const td = document.createElement("td");
+      td.innerHTML = data.count;
+      tr.appendChild(th);
+      tr.appendChild(td);
+      tableEl.appendChild(tr);
+    }
+  
+    resultData.forEach((data) => createTableRow(data));
+  
+    const restartCont = document.createElement("div");
+    restartCont.classList.add("restartCont");
+    const restartBtn = document.createElement("button");
+    restartBtn.innerHTML = `Restart`;
+    restartCont.appendChild(restartBtn);
+  
+    restartBtn.addEventListener("click", (e) => {
+      window.location.reload();
+    });
+  
+    // Append Table to continer
+    quizContainerEl.appendChild(tableEl);
+    quizContainerEl.appendChild(restartCont);
+  }
   
